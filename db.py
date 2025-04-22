341c93d45c45263df9410f943ed1c12b9dfd826a
Javier Agudelo W <javieragu1104@gmail.com>
2025-04-21 19:52:53 -0500

    [dev]:Creación de conexión a la base de datos

---
+++ db.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+from fastapi import Depends,FastAPI
+from sqlmodel import Session, create_engine,SQLModel
+
+sqlite_name = "fantasy_data.db"
+sqlite_url = f"sqlite:///{sqlite_name}"
+
+engine = create_engine(sqlite_url)
+
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+def getSession():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(getSession)]
