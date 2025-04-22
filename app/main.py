fabab5c3617ad95ab785ca8bd50fd464ab9be02c
Javier Agudelo W <javieragu1104@gmail.com>
2025-04-21 19:54:40 -0500

    [dev]:Conexión de la estructura de la BD a la aplicación, y levantamiento de la base de datos

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-#from db import create_all_tables
-#from models import Player, Performance, Team, League, TeamPlayer
+from db import create_all_tables
+from models import Player, Performance, Team, League, TeamPlayer
 
 
-app = FastAPI()
+app = FastAPI(lifespan=create_all_tables)
 
 @app.get('/')
 async def root():
