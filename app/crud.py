656f6a38c42b9ce2e1676dba1c4ab80f670f02cc
Javier Agudelo W <javieragu1104@gmail.com>
2025-04-21 21:26:52 -0500

    [dev]: Se finalizan las funciones en el CRUD

--- app/crud.py
+++ app/crud.py
@@ -1,10 +1,11 @@
 from datetime import date
 from typing import Optional, List
 
-from sqlmodel import Session, select # type: ignore
-from sqlmodel.orm import selectinload # type: ignore
+from sqlmodel import Session, select
+from sqlmodel.orm import selectinload 
 
-from models import Player, Performance
+from models import Player, Performance, League, Team
+from sqlalchemy import func
 
 
 def get_player_by_id(
@@ -21,7 +22,6 @@ def get_player_by_id(
     )
     return db.exec(statement).first()
 
-
 def get_players(
     db: Session,
     skip: int = 0,
@@ -32,24 +32,23 @@ def get_players(
 
     statement = select(Player)
 
-    if min_last_changed_date:
+    if min_last_changed_date is not None:
         statement = statement.where(
             Player.last_changed_date >= min_last_changed_date
         )
 
-    if first_name:
+    if first_name is not None:
         statement = statement.where(
             Player.first_name == first_name
         )
 
-    if last_name:
+    if last_name is not None:
         statement = statement.where(
             Player.last_name == last_name
         )
 
     statement = statement.offset(skip).limit(limit)
     return db.exec(statement).all()
-
 
 def get_performances(
     db: Session,
@@ -59,10 +58,79 @@ def get_performances(
 
     statement = select(Performance)
 
-    if min_last_changed_date:
+    if min_last_changed_date is not None:
         statement = statement.where(
             Performance.last_changed_date >= min_last_changed_date
         )
 
     statement = statement.offset(skip).limit(limit)
     return db.exec(statement).all()
+
+def get_league(db:Session,
+               league_id: int):
+    
+    statement = select(League).where(League.league_id == league_id)
+    return db.exec(statement).first()
+
+def get_leagues(db:Session,
+                skip: int = 0,
+                limit: int = 100,
+                min_last_changed_date: date = None,
+                league_name: str = None) -> List[League]:
+    
+    statement = select(League)
+
+    if min_last_changed_date is not None:
+        statement = statement.where(
+            League.last_changed_date >= min_last_changed_date
+        )
+    
+    if league_name is not None:
+        statement = statement.where(
+            League.league_name == league_name
+        )
+    
+    statement = statement.offset(skip).limit(limit)
+
+    return db.exec(statement).all()
+
+def get_teams(db:Session,
+                skip: int = 0,
+                limit: int = 100,
+                min_last_changed_date: date = None,
+                team_name: str = None,
+                league_id: int = None )-> List[Team]:
+    
+    statement = select(Team)
+
+    if min_last_changed_date is not None:
+        statement = statement.where(
+            Team.last_changed_date >= min_last_changed_date
+        )
+    
+    if team_name is not None:
+        statement = statement.where(
+            Team.team_name == team_name
+        )
+    
+    if league_id is not None:
+        statement = statement.where(
+            Team.league_id == league_id
+        )
+    
+    statement = statement.offset(skip).limit(limit)
+
+    return db.exec(statement).all()
+
+##Analytics Queries
+def get_player_count(db:Session) -> int:
+    statement = select(func.count(Player.player_id))
+    return db.exec(statement).scalar_one()
+
+def get_team_count(db:Session) -> int:
+    statement = select(func.count(Team.team_id))
+    return db.exec(statement).scalar_one()
+
+def get_league_count(db:Session) -> int:
+    statement = select(func.count(League.league_id))
+    return db.exec(statement).scalar_one()
