6c9606a8f503cb3a6063d94eb7b1b84f2d6d49d8
Javier Agudelo W <javieragu1104@gmail.com>
2025-04-22 20:16:33 -0500

    [dev]: Se desarrolla el primer endpint. [fix]: Se arreglan errores en la base de datos

--- db.py
+++ db.py
@@ -15,4 +15,4 @@ def getSession():
     with Session(engine) as session:
         yield session
 
-SessionDep = Annotated[Session, Depends(getSession)] #Permite Llamar facilmente a la base de datos
+#SessionDep = Annotated[Session, Depends(getSession)] #Permite Llamar facilmente a la base de datos
